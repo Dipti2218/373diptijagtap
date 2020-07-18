494c0b254aad7bf8e9dd83b63a8195dbbbf5883d
Dipti2218 <68051833+Dipti2218@users.noreply.github.com>
2020-07-18 13:02:03 +0530

    Create SDL -assignment 1 c.py

---
+++ SDL -assignment 1 c.py
@@ -0,0 +1,13 @@
+n1=int (input ("Enter the first no.="))
+n2=int (input ("Enter the second no.="))
+
+print("N1=",n1)
+print("N2=",n2)
+
+a=n1+n2
+b=n1-n2
+C=n1*n2
+
+print("Addition=",a)
+print("Subtraction=",b)
+print(product=",c)
